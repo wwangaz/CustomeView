1f009c48d9fb96f2d1bb06f0e5f31f6ef2298efe
王维民 <wwangaz@connect.ust.hk>
2016-05-11 11:56:56 +0800

    添加BottomSheetDialog

--- app/src/main/java/com/example/wangweimin/customerview/view/WheelView.java
+++ app/src/main/java/com/example/wangweimin/customerview/view/WheelView.java
@@ -12,7 +12,6 @@ import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
@@ -118,7 +117,7 @@ public class WheelView extends ScrollView {
                             });
                         }
                     }
-                }else {
+                } else {
                     initialY = getScrollY();
                     WheelView.this.postDelayed(scrollTask, newCheck);
                 }
