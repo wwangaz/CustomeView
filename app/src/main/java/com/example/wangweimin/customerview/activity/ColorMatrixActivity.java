eb57283022ac9e9cbeced241617a882387843b69
王维民 <wwangaz@connect.ust.hk>
2017-02-15 15:40:02 +0800

    添加底片，老照片，浮雕效果

--- app/src/main/java/com/example/wangweimin/customerview/activity/ColorMatrixActivity.java
+++ app/src/main/java/com/example/wangweimin/customerview/activity/ColorMatrixActivity.java
@@ -3,6 +3,7 @@ package com.example.wangweimin.customerview.activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
@@ -13,6 +14,7 @@ import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.AppCompatSeekBar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 
@@ -22,6 +24,7 @@ import java.io.IOException;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * Created by wangweimin on 17/2/15.
@@ -46,6 +49,21 @@ public class ColorMatrixActivity extends AppCompatActivity implements SeekBar.On
 
     @BindView(R.id.lum_seekbar)
     AppCompatSeekBar mLumSeekBar;
+
+    @OnClick({R.id.button1, R.id.button2, R.id.button3})
+    void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button1:
+                mImageView.setImageBitmap(handleImageNegative(mBitmap));
+                break;
+            case R.id.button2:
+                mImageView.setImageBitmap(handleImageFade(mBitmap));
+                break;
+            case R.id.button3:
+                mImageView.setImageBitmap(handleImageRelief(mBitmap));
+                break;
+        }
+    }
 
     private float mSaturation = START_VALUE;
 
@@ -102,7 +120,7 @@ public class ColorMatrixActivity extends AppCompatActivity implements SeekBar.On
     }
 
     private Bitmap handleImageEffect(Bitmap bitmap, float hue, float saturation, float lum) {
-        if(bitmap != null) {
+        if (bitmap != null) {
             Bitmap bmp = Bitmap.createBitmap(bitmap.getWidth(), bitmap.getHeight(), Bitmap.Config.ARGB_8888);
             Canvas canvas = new Canvas(bmp);
             Paint paint = new Paint();
@@ -128,6 +146,126 @@ public class ColorMatrixActivity extends AppCompatActivity implements SeekBar.On
             return bmp;
         }
         return null;
+    }
+
+    private Bitmap handleImageNegative(Bitmap bitmap) {
+        if (bitmap != null) {
+            int width = bitmap.getWidth();
+            int height = bitmap.getHeight();
+            int color;
+            int r, g, b, a;
+
+            Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+            int[] oldPx = new int[width * height];
+            int[] newPx = new int[width * height];
+            bitmap.getPixels(oldPx, 0, width, 0, 0, width, height);
+
+            for (int i = 0; i < width * height; i++) {
+                color = oldPx[i];
+                r = Color.red(color);
+                g = Color.green(color);
+                b = Color.blue(color);
+                a = Color.alpha(color);
+
+                r = 255 - r;
+                g = 255 - g;
+                b = 255 - b;
+
+                checkColor(r);
+                checkColor(g);
+                checkColor(b);
+
+                newPx[i] = Color.argb(a, r, g, b);
+            }
+            bmp.setPixels(newPx, 0, width, 0, 0, width, height);
+            return bmp;
+        }
+        return null;
+    }
+
+    private Bitmap handleImageFade(Bitmap bitmap) {
+        if (bitmap != null) {
+            int width = bitmap.getWidth();
+            int height = bitmap.getHeight();
+            int color;
+            int r, g, b, a;
+            int r1, g1, b1;
+
+            Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+            int[] oldPx = new int[width * height];
+            int[] newPx = new int[width * height];
+            bitmap.getPixels(oldPx, 0, width, 0, 0, width, height);
+
+            for (int i = 0; i < width * height; i++) {
+                color = oldPx[i];
+                r = Color.red(color);
+                g = Color.green(color);
+                b = Color.blue(color);
+                a = Color.alpha(color);
+
+                r1 = (int) (0.393 * r + 0.769 * g + 0.189 * b);
+                g1 = (int) (0.349 * r + 0.686 * g + 0.168 * b);
+                b1 = (int) (0.272 * r + 0.534 * g + 0.131 * b);
+
+                checkColor(r1);
+                checkColor(g1);
+                checkColor(b1);
+
+                newPx[i] = Color.argb(a, r1, g1, b1);
+            }
+            bmp.setPixels(newPx, 0, width, 0, 0, width, height);
+            return bmp;
+        }
+        return null;
+    }
+
+    private Bitmap handleImageRelief(Bitmap bitmap) {
+        if (bitmap != null) {
+            int width = bitmap.getWidth();
+            int height = bitmap.getHeight();
+            int color;
+            int r, g, b, a;
+            int rp = 0, gp = 0, bp = 0;
+            int r1, g1, b1;
+
+            Bitmap bmp = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+            int[] oldPx = new int[width * height];
+            int[] newPx = new int[width * height];
+            bitmap.getPixels(oldPx, 0, width, 0, 0, width, height);
+
+            for (int i = 0; i < width * height; i++) {
+                color = oldPx[i];
+                r = Color.red(color);
+                g = Color.green(color);
+                b = Color.blue(color);
+                a = Color.alpha(color);
+
+                r1 = rp - r + 127;
+                g1 = gp - g + 127;
+                b1 = bp - b + 127;
+
+                checkColor(r1);
+                checkColor(g1);
+                checkColor(b1);
+
+                rp = r;
+                gp = g;
+                bp = b;
+
+                newPx[i] = Color.argb(a, r1, g1, b1);
+            }
+            bmp.setPixels(newPx, 0, width, 0, 0, width, height);
+            return bmp;
+        }
+        return null;
+    }
+
+    private int checkColor(int color) {
+        if (color > 255)
+            color = 255;
+        else if (color < 0)
+            color = 0;
+        return color;
     }
 
     @Override
